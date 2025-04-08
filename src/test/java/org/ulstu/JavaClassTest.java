cb8cf491b12a94d682759c20525cd7f4a1161982
Cyust <a_berdyaev@mail.ru>
2025-04-08 21:19:07 +0400

    project

---
+++ src/test/java/org/ulstu/JavaClassTest.java
@@ -0,0 +1,11 @@
+package org.ulstu;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JavaClassTest {
+    @Test
+    public void test() {
+        Assertions.assertEquals(4, 2+2);
+    }
+}
